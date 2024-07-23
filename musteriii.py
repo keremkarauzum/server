f5c4453ef9208d4471579ad0d9049da2bf2af648
keremkarauzum <130603304+keremkarauzum@users.noreply.github.com>
2024-07-23 15:17:26 +0300

    Update musteriii.py

--- musteriii.py
+++ musteriii.py
@@ -1,6 +1,8 @@
 import socket
-host = '192.168.56.1'  
+
+host = '127.0.0.1'  
 port = 5000
+
     
 def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,8 +10,9 @@ def start_client():
     print(f"Sunucuya bağlandı: {host}:{port}")
     mesaj = "Baglanti saglandi"
     client_socket.send(mesaj.encode('utf-8'))
-    response = client_socket.recv(1024).decode('utf-8')
-    print(f"Sunucudan gelen cevap: {response}")
+    while 1:
+     response = client_socket.recv(1024).decode('utf-8')
+     print(response)
     
 
 if __name__ == "__main__":
