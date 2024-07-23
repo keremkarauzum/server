25cc6a6b617cfd8f7acfd8572108475f3f779ac7
keremkarauzum <130603304+keremkarauzum@users.noreply.github.com>
2024-07-23 15:47:13 +0300

    serverrr.py

--- serverrr.py
+++ serverrr.py
@@ -1,7 +1,10 @@
 import socket
+from dronekit import connect
+import mesajlar
+import time
+siha=connect('127.0.0.1:14550',wait_ready=True)
 
-
-host = '192.168.56.1'  
+host = '127.0.0.1'  
 port = 5000
 
 def start_server():
@@ -14,20 +17,41 @@ def start_server():
     print("Baglanti araniyor")
     
     while True:
-        
+         
         client_socket, client_address = sunucu.accept()
         print(f"Bağlantı kabul edildi: {client_address}")
         
-        # İstemciden veri alma
-        data = client_socket.recv(1024).decode('utf-8')
-        print(f"Gelen veri: {data}")
+    
+        veri = client_socket.recv(1024).decode('utf-8')
+        print(f"Gelen veri: {veri}\n")
         
-        
-        response = "Baglanti saglandi"
-        client_socket.send(response.encode('utf-8'))
-        
-        
+        while 1:
+            response = "Baglanti saglandi\n"
+            client_socket.send(response.encode('utf-8'))
+            enlem ,boylam ,irtifa=mesajlar.listen_gps(siha)
+            dikilme,yonelme,yatis=mesajlar.listen_aci(siha)
+            hiz=mesajlar.listen_hiz(siha)
+            batarya=mesajlar.listen_batarya(siha)
+            mesaj1=f"enlem:{enlem}\n"
+            mesaj2=f"boylam:{boylam}\n"
+            mesaj3=f"irtifa:{irtifa}\n"
+            mesaj4=f"dikilme:{dikilme}\n"
+            mesaj5=f"yonelme:{yonelme}\n"
+            mesaj6=f"yatis:{yatis}\n"
+            mesaj7=f"hiz:{hiz}\n"
+            mesaj8=f"batarya:{batarya}\n"
+            client_socket.send(mesaj1.encode('utf-8'))
+            client_socket.send(mesaj2.encode('utf-8'))
+            client_socket.send(mesaj3.encode('utf-8'))
+            client_socket.send(mesaj4.encode('utf-8'))
+            client_socket.send(mesaj5.encode('utf-8'))
+            client_socket.send(mesaj6.encode('utf-8'))
+            client_socket.send(mesaj7.encode('utf-8'))
+            client_socket.send(mesaj8.encode('utf-8'))
+            time.sleep(1)
 
-# Sunucuyu başlat
 if __name__ == "__main__":
     start_server()
+
+
+
